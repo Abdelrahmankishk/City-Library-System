f58745082af5d93c048a849b42290d9678289ff7
Abdelrahmankishk <abdelrahmankishk@gmail.com>
2026-08-14 12:09:40 +0300

    add a IDisplayable interface & add IBroowable to adjust borrowing rules for future borrowed items in addition to the BookCopy

---
+++ City Library System/Contracts/IBorrowable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace City_Library_System.Contracts
+{
+    public interface IBorrowable
+    {
+        void Borrow(Member member, int loanDays = 14);
+        decimal Return();
+        bool isAvailable();
+    }
+}
