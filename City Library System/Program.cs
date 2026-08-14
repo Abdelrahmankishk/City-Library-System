34001f11280867efa37bf9142e26f6574b8dab96
Abdelrahmankishk <abdelrahmankishk@gmail.com>
2026-08-14 11:43:58 +0300

    Add project files.

---
+++ City Library System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace City_Library_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("City Library System");
+        }
+    }
+}
