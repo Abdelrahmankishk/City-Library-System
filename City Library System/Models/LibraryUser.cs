0d3c25baca1e095e61f712dbe36cf6ac492318eb
Abdelrahmankishk <abdelrahmankishk@gmail.com>
2026-08-17 16:11:34 +0300

    add LibraryUser Class as the father of any user in the system will inherit from

---
+++ City Library System/Models/LibraryUser.cs
@@ -0,0 +1,23 @@
+﻿using City_Library_System.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace City_Library_System.Models
+{
+    public abstract class LibraryUser : IDisplayable
+    {
+        public string Name { get; protected set; }
+        public string Phone { get; protected set; }
+
+        public LibraryUser(string name, string phone)
+        {
+            Name = name;
+            Phone = phone;
+        }
+
+        public abstract string Display();
+    }
+}
