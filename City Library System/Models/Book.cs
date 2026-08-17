a4ba80712e4a37218449aeba1a46d4990b4f284e
Abdelrahmankishk <abdelrahmankishk@gmail.com>
2026-08-17 19:08:50 +0300

    add Book Class

---
+++ City Library System/Models/Book.cs
@@ -0,0 +1,36 @@
+﻿using City_Library_System.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace City_Library_System.Models
+{
+    public class Book: IDisplayable
+    {
+        public string ISBN { get; private set; }
+        public string Title { get; private set; }
+        public string AuthorName { get; private set; }
+        public string Category { get; private set; }
+        public int PubYear { get; private set; }
+
+        public Book(string iSBN, string title, string authorName, string category, int pubYear)
+        {
+            ISBN = iSBN;
+            Title = title;
+            AuthorName = authorName;
+            Category = category;
+            PubYear = pubYear;
+        }
+        public Book(string iSBN, string title): this(iSBN,title,"Unknown", "General", 0)
+        {
+
+        }
+
+        public string Display()
+        {
+            return $"[{ISBN}] - {Title} Written by {AuthorName} Category: {Category} PUblished in: {PubYear}";
+        }
+    }
+}
