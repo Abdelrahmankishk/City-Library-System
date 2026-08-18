73fc04c90a6d4ecfbf9499e5bf772241d47a97b7
Abdelrahmankishk <abdelrahmankishk@gmail.com>
2026-08-18 17:55:48 +0300

    add NormalizeId Extention Method to check given ID is the same format as the Classes IDs

---
+++ City Library System/Extentions/StringExtention.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace City_Library_System.Extentions
+{
+    public static class StringExtention
+    {
+        public static string NormalizeID(this string str)
+        {
+            return str?.Trim().ToUpperInvariant() ?? string.Empty;
+        }
+    }
+}
